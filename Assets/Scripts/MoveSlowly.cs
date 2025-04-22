f7802ab370a6401fc85af3c877bcfa784e44f316
michaelw2002 <142000527+MekelWibi@users.noreply.github.com>
2025-04-22 10:59:20 +0700

    Integrate Remote Config to update values in a script without rebuilding the application

--- Assets/Scripts/MoveSlowly.cs
+++ Assets/Scripts/MoveSlowly.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Services.Core;
+using Unity.Services.RemoteConfig;
 
 public class MoveSlowly : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 0.5f; // units per second
+    public struct userAttributes { }
+    public struct appAttributes { }
+
+    [SerializeField] private float moveSpeed = 1.0f;
+
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        RemoteConfigService.Instance.FetchCompleted += ApplyRemoteConfig;
+        RemoteConfigService.Instance.FetchConfigs(new userAttributes(), new appAttributes());
+    }
+
+    private void ApplyRemoteConfig(ConfigResponse response)
+    {
+        if (RemoteConfigService.Instance.appConfig.HasKey("cubeSpeed"))
+        {
+            moveSpeed = RemoteConfigService.Instance.appConfig.GetFloat("cubeSpeed");
+            Debug.Log("Remote config moveUpSpeed: " + moveSpeed);
+        }
+    }
 
     void Update()
     {
-        // Move upward every frame at moveSpeed
-        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+        transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
     }
 }
