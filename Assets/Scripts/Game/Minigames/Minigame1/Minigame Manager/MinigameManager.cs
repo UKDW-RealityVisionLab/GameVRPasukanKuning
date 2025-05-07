7c0bc1a4b5f126c06a7dd7dc91df5aa8bd66f3ba
MaverickMM <78334716+MaverickMM@users.noreply.github.com>
2025-05-07 16:31:58 +0700

    Add UI Counter
    - Added UI Counter for Drowned

--- Assets/Scripts/Game/Minigames/Minigame1/Minigame Manager/MinigameManager.cs
+++ Assets/Scripts/Game/Minigames/Minigame1/Minigame Manager/MinigameManager.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using TMPro;
 
 public class MinigameManager : MonoBehaviour
 {
     public NPCFSMController[] npcs;
+    public TMP_Text drownedCounterText; // Assign in inspector
 
     private int drownedCount = 0;
 
@@ -12,17 +14,26 @@ public class MinigameManager : MonoBehaviour
         {
             npc.OnDrowned += HandleNPCDrowned;
         }
+
+        UpdateDrownedText();
     }
 
     void HandleNPCDrowned()
     {
         drownedCount++;
         Debug.Log("NPC drowned. Total: " + drownedCount);
+        UpdateDrownedText();
 
         if (drownedCount >= 3)
         {
             Debug.Log("All NPCs drowned!");
-            // Trigger fail state, UI, etc.
+            // TODO: Trigger fail state or show Game Over
         }
+    }
+
+    void UpdateDrownedText()
+    {
+        if (drownedCounterText != null)
+            drownedCounterText.text = $"Drowned: {drownedCount} / {npcs.Length}";
     }
 }
