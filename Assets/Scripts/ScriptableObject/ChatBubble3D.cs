910f141011d2ac510a20bcf41a57d3aa7c7e730c
Riko <92403924+ViGi9029@users.noreply.github.com>
2025-07-31 17:32:08 +0700

    Revert "Merge branch 'NPC-Interaction'"
    This reverts commit 16ceb53105c0f0af84f4470cbff1ba28ae294dc3, reversing
    changes made to 43ffece2fb0d62ba0b305d74b7c35478c2b565de.

--- Assets/Scripts/ScriptableObject/ChatBubble3D.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ChatBubble", menuName = "Chat/Chat Bubble")]
-public class ChatBubble3D : ScriptableObject
-{
-    public string id;
-    public string displayName;
-    [HideInInspector] public bool isComplete;
-}
